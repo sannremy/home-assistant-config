6709d99ceddb51a35344ccc080346518d89fe888
Sann-Remy Chea <667617+sannremy@users.noreply.github.com>
2021-10-24 21:37:16 +0200

    Added router IP

--- shell/r7000_backup.sh
+++ shell/r7000_backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Usage:
-# ./r7000_backup.sh username:password
+# ./r7000_backup.sh username:password router_ip
 
 folder="/share/r7000"
-target="http://routerlogin.com/NETGEAR_R7000.cfg"
+target="http://$2/NETGEAR_R7000.cfg"
 
 credentials=$(echo -ne $1 | base64)
 date=$(date +"%d-%m-%Y")
