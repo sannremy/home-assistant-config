4a09d55f2890475cf043bdf65dbb40037f897f2b
Sann-Remy Chea <667617+sannremy@users.noreply.github.com>
2021-10-25 20:47:45 +0200

    Fixed R7000 auto-backup script

--- shell/r7000_backup.sh
+++ shell/r7000_backup.sh
@@ -4,12 +4,24 @@
 # ./r7000_backup.sh username:password router_ip
 
 folder="/share/r7000"
-target="http://$2/NETGEAR_R7000.cfg"
+hostname=$2
+
+target_cookie="http://${hostname}/"
+target_config="http://${hostname}/NETGEAR_R7000.cfg"
 
 credentials=$(echo -ne $1 | base64)
 date=$(date +"%d-%m-%Y")
 dest="${folder}/backup-$date.cfg"
 
-curl $target \
+# Get XSRF token from cookie
+curl -s -c cookie.txt $target_cookie > /dev/null
+token=$(tail -n1 cookie.txt  | awk '{print $NF}')
+
+# Download R7000 config
+curl $target_config \
     -H "Authorization: Basic ${credentials}" \
+    -H "Cookie: XSRF_TOKEN=${token}" \
     --output $dest
+
+# Clean up
+rm cookie.txt
