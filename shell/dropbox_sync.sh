8d0875dc06220b707e2867ed7f0693b23008cca5
Sann-Remy Chea <667617+sannremy@users.noreply.github.com>
2023-02-09 21:13:53 +0100

    Debug Dropbox sync

--- shell/dropbox_sync.sh
+++ shell/dropbox_sync.sh
@@ -17,38 +17,38 @@ curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dr
 chmod +x dropbox_uploader.sh
 
 # Compare files in dropbox and local folder
-remote_list=$(./dropbox_uploader.sh -f $config_file list $remote_backup_folder)
-local_list=($(ls -1 -t $local_backup_folder | head -n $keep_last))
+# remote_list=$(./dropbox_uploader.sh -f $config_file list $remote_backup_folder)
+# local_list=($(ls -1 -t $local_backup_folder | head -n $keep_last))
 
-# Upload sorted local files to dropbox (most recent files)
-for file_name in "${local_list[@]}"; do
-    # Upload file to dropbox
-    ./dropbox_uploader.sh -q -s -f $config_file upload $local_backup_folder/$file_name $remote_backup_folder
-done
+# # Upload sorted local files to dropbox (most recent files)
+# for file_name in "${local_list[@]}"; do
+#     # Upload file to dropbox
+#     ./dropbox_uploader.sh -q -s -f $config_file upload $local_backup_folder/$file_name $remote_backup_folder
+# done
 
-# Loop through remote_list
-index=0
-while read -r line; do
-    # Skip first line
-    if [ $index -eq 0 ]; then
-        index=1
-        continue
-    fi
+# # Loop through remote_list
+# index=0
+# while read -r line; do
+#     # Skip first line
+#     if [ $index -eq 0 ]; then
+#         index=1
+#         continue
+#     fi
 
-    # Increment index
-    index=$((index+1))
+#     # Increment index
+#     index=$((index+1))
 
-    # Get file name
-    file_name=$(echo $line | awk '{print $3}')
+#     # Get file name
+#     file_name=$(echo $line | awk '{print $3}')
 
-    # Check if file exists in local_list array
-    if [[ " ${local_list[@]} " =~ " $file_name " ]]; then
-        # File exists in local folder
-    else
-        # Delete file from dropbox
-        ./dropbox_uploader.sh -q -f $config_file delete $file_name
-    fi
-done <<< "$remote_list"
+#     # Check if file exists in local_list array
+#     if [[ " ${local_list[@]} " =~ " $file_name " ]]; then
+#         # File exists in local folder
+#     else
+#         # Delete file from dropbox
+#         ./dropbox_uploader.sh -q -f $config_file delete $file_name
+#     fi
+# done <<< "$remote_list"
 
-# Clean up
-rm dropbox_uploader.sh
+# # Clean up
+# rm dropbox_uploader.sh
