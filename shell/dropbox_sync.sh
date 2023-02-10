9bed8384588ebb70acf244a855fafe9b8e15860f
Sann-Remy Chea <667617+sannremy@users.noreply.github.com>
2023-02-10 20:06:29 +0100

    Debug Dropbox sync

--- shell/dropbox_sync.sh
+++ shell/dropbox_sync.sh
@@ -18,13 +18,13 @@ chmod +x dropbox_uploader.sh
 
 # Compare files in dropbox and local folder
 remote_list=$(./dropbox_uploader.sh -f $config_file list $remote_backup_folder)
-local_list=($(ls -d $local_backup_folder/* | head -n $keep_last))
+local_list=$(ls -d $local_backup_folder/* | head -n $keep_last)
 
 # Upload sorted local files to dropbox (most recent files)
-for file_name in "${local_list[@]}"; do
+while read -r line; do
     # Upload file to dropbox
-    ./dropbox_uploader.sh -q -s -f $config_file upload $file_name $remote_backup_folder
-done
+    ./dropbox_uploader.sh -q -s -f $config_file upload $line $remote_backup_folder
+done <<< "$local_list"
 
 # Loop through remote_list
 index=0
@@ -41,8 +41,8 @@ while read -r line; do
     # Get file name
     file_name=$(echo $line | awk '{print $3}')
 
-    # Check if file exists in local_list array
-    if [[ " ${local_list[@]} " =~ " $file_name " ]]; then
+    # Check if file exists in local_list
+    if echo $local_list | grep -q $file_name; then
         # File exists in local folder
     else
         # Delete file from dropbox
