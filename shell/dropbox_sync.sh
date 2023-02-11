09a4edf045f8c6d9414c2a21260eb7d79ff1b6e6
Sann-Remy Chea <667617+sannremy@users.noreply.github.com>
2023-02-11 07:46:15 +0100

    Debug Dropbox sync

--- shell/dropbox_sync.sh
+++ shell/dropbox_sync.sh
@@ -5,12 +5,11 @@
 #   2. Copy the config file to the home directory AS /config/shell/.dropbox_uploader
 
 # Usage:
-# ./dropbox_sync.sh local_backup_folder remote_backup_folder keep_last
+# ./dropbox_sync.sh remote_backup_folder keep_last
 
 config_file="/config/shell/.dropbox_uploader"
-local_backup_folder=$1
-remote_backup_folder=$2
-keep_last=$3
+remote_backup_folder=$1
+keep_last=$2
 
 # Download dropbox_uploader.sh and make it executable
 curl -s "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
@@ -18,13 +17,19 @@ chmod +x dropbox_uploader.sh
 
 # Compare files in dropbox and local folder
 remote_list=$(./dropbox_uploader.sh -f $config_file list $remote_backup_folder)
-local_list=$(ls -d $local_backup_folder/* | head -n $keep_last)
-local_list=$(curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/backups | jq -r '.data.backups |= sort_by(.date)' | jq -r '[.data.backups[] | "'$local_backup_folder'" + .slug + ".tar"] | reverse | .[]' | head -n $keep_last)
+local_list=$(curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/backups | jq -r '.data.backups |= sort_by(.date)' | jq -r '[.data.backups[] | .slug] | reverse | .[]' | head -n $keep_last)
 
 # Upload sorted local files to dropbox (most recent files)
-while read -r line; do
+while read -r slug; do
+    output="/tmp/$slug.tar"
+    # Fetch backup file from supervisor
+    curl -s -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/backups/$slug/download --output $output
+
     # Upload file to dropbox
-    ./dropbox_uploader.sh -q -s -f $config_file upload $line $remote_backup_folder
+    ./dropbox_uploader.sh -q -s -f $config_file upload $output $remote_backup_folder
+
+    # Clean up
+    rm $output
 done <<< "$local_list"
 
 # Loop through remote_list
