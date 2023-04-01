549a1f81c82113d725e99316245aa63b4b773ecf
Sann-Remy Chea <667617+sannremy@users.noreply.github.com>
2023-04-01 08:50:17 +0200

    Added sshpass

--- shell/r7000_backup.sh
+++ shell/r7000_backup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# Backup Netgear R7000 router config via SSH with authorized keys (Fresh Tomato)
+# Backup Netgear R7000 router config via SSH (Fresh Tomato)
 # Usage:
-# ./r7000_backup.sh username router_ip
+# ./r7000_backup.sh username password router_ip
 
 username=$1
-router_ip=$2
+password=$2
+router_ip=$3
 
 date=$(date +"%d-%m-%Y")
 folder="/share/r7000"
 file="backup-$date.cfg"
 
-ssh $username@$router_ip "nvram save $file"
-scp $username@$router_ip:$file $folder
-ssh $username@$router_ip "rm $file"
+apk add sshpass
+sshpass -p $password $username@$router_ip "nvram save $file"
+sshpass -p $password scp $username@$router_ip:$file $folder
+sshpass -p $password $username@$router_ip "rm $file"
