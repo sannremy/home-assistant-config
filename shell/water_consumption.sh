869869e2d6b0929bfadff0206834be65b8401e1b
Sann-Remy Chea <667617+sannremy@users.noreply.github.com>
2023-05-24 06:42:00 +0200

    Fixed Suez

--- shell/water_consumption.sh
+++ shell/water_consumption.sh
@@ -2,12 +2,23 @@
 
 # Get Suez (Tout sur mon eau) current month water consumption
 # Usage:
-# ./water_consumption.sh username password
+# ./water_consumption.sh username password meter_id
 
 username=$1
 password=$2
 meter_id=$3
 
-apk add -q chromium nodejs-current npm
+# Install chromium, nodejs and dependencies
+apk add -q \
+    chromium \
+    nss \
+    freetype \
+    harfbuzz \
+    ca-certificates \
+    ttf-freefont \
+    nodejs-current npm
+
+# Install dependencies
 cd /config/shell/node && npm install --silent &>/dev/null
+
 SUEZ_USERNAME=$username SUEZ_PASSWORD=$password SUEZ_METER_ID=$meter_id node /config/shell/node/suez.js > /config/shell/output/water_consumption.txt
