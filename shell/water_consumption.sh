875f4cf6142f9045844f175353545d50c762b6a1
Sann-Remy Chea <667617+sannremy@users.noreply.github.com>
2023-05-26 21:25:04 +0200

    Fixed Suez

--- shell/water_consumption.sh
+++ shell/water_consumption.sh
@@ -14,4 +14,8 @@ apk add -q chromium nodejs-current npm
 # Install dependencies
 cd /config/shell/node && npm install --silent &>/dev/null
 
-SUEZ_USERNAME=$username SUEZ_PASSWORD=$password SUEZ_METER_ID=$meter_id node /config/shell/node/suez.js > /config/shell/output/water_consumption.txt
+uname -a > /config/shell/output/uname.txt
+whoami > /config/shell/output/uname.txt
+which chromium-browser > /config/shell/output/uname.txt
+
+# SUEZ_USERNAME=$username SUEZ_PASSWORD=$password SUEZ_METER_ID=$meter_id node /config/shell/node/suez.js > /config/shell/output/water_consumption.txt
